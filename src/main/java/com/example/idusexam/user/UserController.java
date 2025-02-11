00ecdfd03535e8e0c853de7b63f99b79c415982a
InukChoi <iu.choi3197@gmail.com>
2025-02-11 11:12:49 +0900

    [Feat] 회원가입 기능 추가 #2

--- src/main/java/com/example/idusexam/user/UserController.java
+++ src/main/java/com/example/idusexam/user/UserController.java
@@ -1,4 +1,21 @@
 package com.example.idusexam.user;
 
+import com.example.idusexam.user.model.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserDto.SignupRequest dto) {
+        userService.registerUser(dto.toEntity());
+        return "success";
+    }
 }
--- src/main/java/com/example/idusexam/user/model/UserDto.java
+++ src/main/java/com/example/idusexam/user/model/UserDto.java
@@ -1,4 +1,32 @@
 package com.example.idusexam.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class UserDto {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SignupRequest {
+        private String username;
+        private String nickname;
+        private String email;
+        private String password;
+        private String gender;
+        private Long phone;
+        public User toEntity() {
+            return User.builder()
+                    .username(username)
+                    .nickname(nickname)
+                    .email(email)
+                    .password(password)
+                    .gender(gender)
+                    .phone(phone)
+                    .build();
+        }
+    }
+
 }
