325539a282e9e2ece45cdd37e9aa95a8729c5835
InukChoi <iu.choi3197@gmail.com>
2025-02-11 11:41:33 +0900

    [Feat] 로그인 기능 추가 #2

--- src/main/java/com/example/idusexam/user/model/User.java
+++ src/main/java/com/example/idusexam/user/model/User.java
@@ -6,7 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Builder
@@ -14,7 +19,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
@@ -27,4 +32,39 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Order> orderList;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+//        GrantedAuthority authority = new GrantedAuthority() {
+//            @Override
+//            public String getAuthority() {
+//                return role;
+//            }
+//        };
+
+        authorities.add(authority);
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
